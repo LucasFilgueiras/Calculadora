dd237d04fdff43752756511ccb697b01d9459a98
Lucas Filgueiras <lucks_fil@hotmail.com>
2022-04-20 18:37:38 -0300

    refatoracao

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,33 @@
 let deleteAll = document.querySelector(".deleteAll");
 let deleteLast = document.querySelector(".deleteLast");
 
-function insert(number) {
-    let num = document.getElementById("resultado").innerHTML;
-    document.getElementById("resultado").innerHTML = num + number;
-}
-
-deleteAll.addEventListener("click", () => {
-    document.querySelector("#resultado").innerHTML = '';
-})
-
-deleteLast.addEventListener("click", () => {
-    let resultado = document.querySelector("#resultado").innerHTML;
-    document.querySelector("#resultado").innerHTML = resultado.substring(0, resultado.length -1)
-})
-
-function calcular() {
-    let resultado = document.querySelector("#resultado").innerHTML;
-    if (resultado) {
-        document.getElementById("resultado").innerHTML = eval(resultado);
-    } else {
-        window.alert("Erro no cálculo, tente novamente.");
+const actions = {
+    deleteAll() {
+        deleteAll.addEventListener("click", () => {
+            document.querySelector("#resultado").innerHTML = '';
+        })
+    },
+    deleteLast() {
+        deleteLast.addEventListener("click", () => {
+            let resultado = document.querySelector("#resultado").innerHTML;
+            document.querySelector("#resultado").innerHTML = resultado.substring(0, resultado.length - 1)
+        })
+    },
+    calculate() {
+        let resultado = document.querySelector("#resultado").innerHTML;
+        if (resultado) {
+            document.getElementById("resultado").innerHTML = eval(resultado);
+        } else {
+            window.alert("Erro no cálculo, tente novamente.");
+        }
+    },
+    insert() {
+        let num = document.getElementById("resultado").innerHTML;
+        document.getElementById("resultado").innerHTML = num + number;
     }
 }
+
+actions.deleteAll();
+actions.deleteLast();
+actions.calculate();
+actions.insert();
